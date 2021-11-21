a95eeee7a15fb58e2291de975566823d49d7e476
Temitope <adesiyantope2014@gmail.com>
2021-11-21 09:56:25 +0100

    21-11-2021

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 var openToggle = document.querySelector('.bars');
 var sidebar = document.querySelector('.sidebar');
-// var closebtn = document.querySelector('.closebtn')
+
 var closebtn2 = document.querySelector('.closebtn2')
 var newSidebar = document.querySelector('.new-sidebar')
+
+
+var toggleButton = document.querySelector('.toggle-button')
+var hiddenForm = document.querySelector('.hidden')
+
+// console.log(hiddenForm)
+
+toggleButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  hiddenForm.classList.toggle('toggleform');
+  toggleButton.innerHTML = ''
+})
+
+
+
 
 
 openToggle.addEventListener('click', function () {
@@ -15,41 +30,12 @@ openToggle.addEventListener('click', function () {
 // });
 
 
-closebtn2.addEventListener('click', function() {
-  newSidebar.style.transform = "translateX(1000%)";
+// closebtn2.addEventListener('click', function() {
+//   newSidebar.style.transform = "translateX(1000%)";
   
-})
+// })
 
 
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
 
 
 
